f433fbb7ea094148beba486154c505ca47d21f8b
Alex Newbegin <alex.newbegin95@gmail.com>
2023-02-22 08:51:50 -0500

    created and finished merging exercises

--- merging_joining.py
+++ merging_joining.py
@@ -39,8 +39,132 @@ merged_df = pd.merge(
 print(merged_df.sample(15))
 
 ## Let's explore more with some flubbed data
-raw_data_1 = {
+player_name_dict = {
     'player_id' : ['1', '2', '3', '4', '5', '6', '7']
-    ,'first_name' : ['Lebron', 'Dirk', 'Vince', 'Enes', 'Derrick', 'Paul']
-    ,'last_name' : ['James', 'Nowitzki', 'Carter', 'Kanter', 'Rose', 'George']
+    ,'first_name' : ['Lebron', 'Dirk', 'Vince', 'Enes', 'Fred', 'Derrick', 'Paul']
+    ,'last_name' : ['James', 'Nowitzki', 'Carter', 'Kanter', 'Van Fleet', 'Rose', 'George']
 }
+player_career_ppg_dict = {
+    'player_id' : ['1', '2', '3', '4', '5']
+    ,'career_ppg' : [27.2, 20.7, 17.2, 11.9, 18.8]
+}
+player_name_df_2 = pd.DataFrame(
+    player_name_dict
+    ,columns= [
+        'player_id'
+        ,'first_name'
+        ,'last_name'
+    ]
+)
+player_career_ppg_df = pd.DataFrame(
+    player_career_ppg_dict
+    ,columns= [
+        'player_id'
+        ,'career_ppg'
+    ]
+)
+
+## Left merge: just like an outer join. Keep all records
+## in left dataframe and append any matching records in
+## right dataframe based on "on" column. Any missing vals
+## will result in NaN values.
+left_merged_df = pd.merge(
+    player_name_df_2
+    ,player_career_ppg_df
+    ,on='player_id'
+    ,how='left'
+)
+
+## Because player_career_ppg_df doesn't have any values 
+## for 6 or 7, those get NaN.
+# print(left_merged_df)
+
+## Right merge will do the same thing except it will
+## keep all right df records and drop any non-matching
+## left df records.
+right_merged_df = pd.merge(
+    player_name_df_2
+    ,player_career_ppg_df
+    ,on='player_id'
+    ,how='right'
+)
+
+## This merge ony shows records 1-5 since those are the 
+## only records in the right df.
+# print(right_merged_df)
+
+## We can do an outer merge which will produce all records
+## and return NaN for anything with unmatched vals. Results
+## here will be similar to left merge we did previously.
+outer_merged_df = pd.merge(
+    player_name_df_2
+    ,player_career_ppg_df
+    ,on='player_id'
+    ,how='outer'
+)
+print(outer_merged_df)
+
+## What if the keys we're joining on between tables are 
+## different? Merge method can take left_on and right_on
+## arguments to handle these situations.
+## Let's get some more dummy data with mismatched key names.
+player_name_dict_2 = {
+    'id' : ['1', '2', '3', '4', '5', '6', '7']
+    ,'first_name' : ['Lebron', 'Dirk', 'Vince', 'Enes', 'Fred', 'Derrick', 'Paul']
+    ,'last_name' : ['James', 'Nowitzki', 'Carter', 'Kanter', 'Van Fleet', 'Rose', 'George']
+}
+player_career_ppg_dict_2 = {
+    'player_id' : ['1', '2', '3', '4', '5']
+    ,'career_ppg' : [27.2, 20.7, 17.2, 11.9, 18.8]
+}
+player_name_df_3 = pd.DataFrame(
+    player_name_dict_2
+    ,columns= [
+        'id'
+        ,'first_name'
+        ,'last_name'
+    ]
+)
+player_career_ppg_df_2 = pd.DataFrame(
+    player_career_ppg_dict_2
+    ,columns= [
+        'player_id'
+        ,'career_ppg'
+    ]
+)
+
+## We can include the left / right on arguments to merge 
+## these two dataframes.
+mismatched_on_merge_df = pd.merge(
+    player_name_df_3
+    ,player_career_ppg_df_2
+    ,how = 'right'
+    ,left_on = 'id'
+    ,right_on = 'player_id'
+)
+
+## This will produce our desired join but we also
+## get both ID columns. 
+# print(mismatched_on_merge_df)
+
+## We can use the drop method to get rid of a
+## column by passing the name and axis.
+mismatched_on_merge_df = pd.merge(
+    player_name_df_3
+    ,player_career_ppg_df_2
+    ,how = 'right'
+    ,left_on = 'id'
+    ,right_on = 'player_id'
+).drop('id', axis=1)
+
+print(mismatched_on_merge_df)
+
+## We can also call a merge directly on a dataframe
+## rather than calling the pandas object.
+direct_left_merge = player_name_df_3.merge(
+    player_career_ppg_df_2
+    ,how='left'
+    ,left_on = 'id'
+    ,right_on = 'player_id'
+    ).drop('id', axis=1)
+print(direct_left_merge)

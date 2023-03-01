298a4b1fc1c952c51c308ce8b300299f0e294a1f
Alex Newbegin <alex.newbegin95@gmail.com>
2023-03-01 08:19:34 -0500

    created our calc distance function and tested it on three players

--- nba-stats-csv/fantasy_model.py
+++ nba-stats-csv/fantasy_model.py
@@ -61,9 +61,7 @@ df_no_outliers = df_cleaned[df_cleaned['gp'] > min_gp]
 # accuracy plus more adoption of three pointers, leading to more scoring. In order to compare across seasons,
 # we need to normalize data to compare between decades. What we want is a funciton that factors min and max
 # values from each season and normalizes all stats from all seasons.
-df_18 = df_no_outliers[df_no_outliers['season_id'] == '2017-18']
-
-# Next, let's normalize points in a new col. We're going to take the value, subtract the minumum value, and divide
+# Let's normalize points in a new col. We're going to take the value, subtract the minumum value, and divide
 # by difference between max and min values, and let's do it as a function.
 
 
@@ -83,5 +81,33 @@ def normalize_df(df):
 # Finally, we need to do this for each season, which can be tricky to pick out the mean, min, and max for each season.
 # Let's group by season_id and then run function.
 df_normalized = grouped_df = df_no_outliers.groupby(
-    ['season_id']).apply(normalize_df)
-print(df_normalized.sample(10))
+    ['season_id'], group_keys=True).apply(normalize_df)
+
+# Now, we need to calculate player distance. This is also referred to as percent error, and shares how close players' 
+# stats are. Our goal is to find the ten players with the shortest distance across all stats.
+# We'll create a function called called calc_distance that takes two players as inputs and outputs their distance.
+# To measure distance, we're going to use the euclidian distance between two points. This measures the length of the 
+# distance between the two points. We can caluclate this with the square root function.
+
+
+def calc_distance(u, v):
+    dist = np.sqrt(np.sum((u - v) ** 2))
+    return dist
+
+# First we'll test two players and see how our function works. Let's join our df_normalized with a df
+# with names to see names.
+df_player_names = pd.read_csv('nbadata/nba-stats-csv/player_id_player_name.csv')
+df_normalized = pd.merge(df_normalized, df_player_names, on = 'player_id', how='left')
+col_list = df_normalized.columns.tolist()
+col_list = col_list[0:1] + col_list[-1:] + col_list[1:-1]
+df_normalized = df_normalized[col_list]
+
+## Let's experiment by getting just Damien Lillard, Steph Curry, and The Stifle Tower in 2017-18
+some_players_list = ['Steph Curry', 'Damian Lillard', 'Rudy Gobert']
+dame_2019 = df_normalized[(df_normalized['player_name'] == 'Damian Lillard') & (df_normalized['season_id'] == '2018-19')] 
+steph_2019 = df_normalized[(df_normalized['player_name'] == 'Stephen Curry') & (df_normalized['season_id'] == '2018-19')] 
+stifle_2019 = df_normalized[(df_normalized['player_name'] == 'Rudy Gobert') & (df_normalized['season_id'] == '2018-19')] 
+dame_2019_ppg = dame_2019.pts.tolist()[0]
+steph_2019_ppg = steph_2019.pts.tolist()[0]
+stifle_2019_ppg = stifle_2019.pts.tolist()[0]
+print(calc_distance(dame_2019_ppg, steph_2019_ppg))
